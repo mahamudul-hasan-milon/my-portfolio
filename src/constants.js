03f35496c41fa4c8e4a30fb099eba853c7cafd7f
mahamudul-hasan-milon <mahamudulhasanmilon553@gmail.com>
2025-09-09 15:51:11 +0600

    added some resources

--- src/constants.js
+++ src/constants.js
@@ -34,25 +34,17 @@ import postgreLogo from "./assets/tech_logo/postgre.png";
 import csharpLogo from "./assets/tech_logo/csharp.png";
 
 // Experience Section Logo's
-import webverseLogo from "./assets/company_logo/webverse_logo.png";
-import agcLogo from "./assets/company_logo/agc_logo.png";
-import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.png";
+import myself from "./assets/profile2.jpg";
+import cholobazar from "./assets/company_logo/cholobazar.png";
 
 // Education Section Logo's
-import glaLogo from "./assets/education_logo/gla_logo.png";
-import bsaLogo from "./assets/education_logo/bsa_logo.png";
-import vpsLogo from "./assets/education_logo/vps_logo.png";
+import green from "./assets/education_logo/green.png";
+import dinajpur from "./assets/education_logo/dinajpur.png";
+import jagonnathpur_logo from "./assets/education_logo/jagonnathpur_logo.jpg";
 
 // Project Section Logo's
-import githubdetLogo from "./assets/work_logo/github_det.png";
-import csprepLogo from "./assets/work_logo/cs_prep.png";
-import movierecLogo from "./assets/work_logo/movie_rec.png";
-import taskremLogo from "./assets/work_logo/task_rem.png";
-import npmLogo from "./assets/work_logo/npm.png";
-import webverLogo from "./assets/work_logo/web_dig.png";
-import cmLogo from "./assets/work_logo/cm.png";
-import imagesearchLogo from "./assets/work_logo/image_search.png";
-import removebgLogo from "./assets/work_logo/remove_bg.png";
+import cat from "./assets/work_logo/cat.png";
+import house from "./assets/work_logo/house.png";
 
 export const SkillsInfo = [
   {
@@ -114,11 +106,11 @@ export const SkillsInfo = [
 export const experiences = [
   {
     id: 0,
-    img: webverseLogo,
-    role: "Fullstack Developer",
-    company: "Webverse Digital",
-    date: "April 2024 - Present",
-    desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+    img: cholobazar,
+    role: "MERN Stack Developer",
+    company: "Academic Project",
+    date: "June 2024 - November 2024",
+    desc: "I built the Cholobazar E-commerce Website using the MERN stack, featuring user login, product, cart and order functionalities. I designed a responsive UI with React.js and Tailwind CSS, integrated REST APIs and managed the project with Git/GitHub.",
     skills: [
       "HTML",
       "CSS",
@@ -134,11 +126,11 @@ export const experiences = [
   },
   {
     id: 1,
-    img: agcLogo,
-    role: "Fullstack Engineer",
-    company: "Agumentik Group of Companies",
-    date: "July 2023 - March 2024",
-    desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    img: myself,
+    role: "Front-End Web Developer",
+    company: "Self-Project",
+    date: "January 2024 - May 2024",
+    desc: "I created a portfolio website using React.js and Tailwind CSS, featuring responsive layouts with reusable components and smooth animations for enhanced UX. I deployed the site for a professional presence and managed hosting and version control with Git/GitHub.",
     skills: [
       "ReactJS",
       "Redux",
@@ -149,63 +141,45 @@ export const experiences = [
       "SQL",
     ],
   },
-  {
-    id: 2,
-    img: newtonschoolLogo,
-    role: "Frontend Intern",
-    company: "Newton School",
-    date: "September 2021 - August 2022",
-    desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
-    skills: ["HTML", "CSS", "Javascript", "Bootstrap", "Figma", "Material UI"],
-  },
 ];
 
 export const education = [
   {
     id: 0,
-    img: glaLogo,
-    school: "GLA University, Mathura",
-    date: "Sept 2022 - July 2024",
-    grade: "7.81 CGPA",
-    desc: "I have completed my Master's degree (MCA) in Computer Applications from GLA University, Mathura. During my time at GLA, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at GLA University has been instrumental in shaping my technical abilities and professional growth.",
-    degree: "Master of Computer Applications - MCA",
+    img: green,
+    school: "Green University of Bangladesh, Rupganj, Narayanganj",
+    date: "December 2021 - December 2025",
+    grade: "3.18 CGPA",
+    desc: "I have completed my Bachelor's degree in Computer Science and Engineering (CSE) from Green University of Bangladesh. Throughout my studies, I gained a strong foundation in programming, software development and computer science principles. I studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development and Software Engineering. I also worked on academic projects and participated in technical workshops, which enhanced my practical skills and prepared me for a career in the software industry.",
+    degree: "Bachelor of Computer Science & Engineering",
   },
   {
     id: 1,
-    img: bsaLogo,
-    school: "BSA College, Mathura",
-    date: "Sept 2018 - Aug 2021",
-    grade: "73.2%",
-    desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
-    degree: "Bachelor of Science - BSC (Computer Science)",
+    img: dinajpur,
+    school: "Dinajpur Government College, Dinajpur",
+    date: "June 2017 - July 2020",
+    grade: "A+",
+    desc: "I have completed my Higher Secondary Certificate (HSC) in Science from Dinajpur Government College. During my studies, I developed a strong foundation in core subjects such as Physics, Chemistry, Mathematics and Biology. This academic background enhanced my analytical thinking, problem-solving ability, and logical reasoning skills. I actively participated in various academic and extracurricular activities, which helped me build discipline, teamwork and leadership qualities. My time at Dinajpur Government College played an important role in shaping my academic journey and preparing me for higher studies in Computer Science and Engineering.",
+    degree: "Higher Secondary Certificate (HSC) in Science",
   },
   {
     id: 2,
-    img: vpsLogo,
-    school: "Vatsalya Public School Govardhan, Mathura",
-    date: "Apr 2017 - March 2018",
-    grade: "78%",
-    desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
-    degree: "CBSE(XII) - PCM with Computer Science",
-  },
-  {
-    id: 3,
-    img: vpsLogo,
-    school: "Vatsalya Public School Govardhan, Mathura",
-    date: "Apr 2015 - March 2016",
-    grade: "87.5%",
-    desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
-    degree: "CBSE(X), Science with Computer Application",
+    img: jagonnathpur_logo,
+    school: "Jagonnathpur High School, Chirirbandor",
+    date: "January 2015 - March 2017",
+    grade: "A+",
+    desc: "Completed Secondary School Certificate (SSC) in Science from Jagonnathpur High School, with a strong foundation in Physics, Chemistry, Mathematics, and General Science that developed my logical reasoning and problem-solving skills.",
+    degree: "Secondary School Certificate (SSC) in Science",
   },
 ];
 
 export const projects = [
   {
     id: 0,
-    title: "GitHub Profile Detective",
+    title: "My Portfolio Website",
     description:
-      "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
-    image: githubdetLogo,
+      "A personal portfolio website built with React.js and Tailwind CSS, featuring reusable components, smooth animations and responsive layouts to highlight skills, projects and achievements professionally.",
+    image: myself,
     tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
     github:
       "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
@@ -213,10 +187,10 @@ export const projects = [
   },
   {
     id: 1,
-    title: "CS Prep",
+    title: "Cholobazar - E-commerce Website",
     description:
-      "A full-stack quiz-based platform designed for GATE/UGC NET students to practice previous year questions and create customized tests. The platform offers comprehensive profile stats and detailed results, helping students track and improve their preparation journey effectively.",
-    image: csprepLogo,
+      " Cholobazar is a full-featured MERN stack e-commerce site with user authentication, product management, shopping cart and responsive UI built using React.js and Tailwind CSS.",
+    image: cholobazar,
     tags: [
       "React JS",
       "Node.js",
@@ -231,72 +205,22 @@ export const projects = [
   },
   {
     id: 2,
-    title: "Movie Recommendation App",
+    title: "Pet Adoption Website",
     description:
-      "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
-    image: movierecLogo,
+      "Created a responsive pet adoption website using HTML, CSS and JavaScript (ES6), featuring pet listings, adoption guidance, shelter directory, application tracking and care resources with dynamic interactions via arrow functions and callbacks.",
+    image: cat,
     tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
     github: "https://github.com/codingmastr/Movie-Recommendation-App",
     webapp: "https://movie-recommendation-app-jet.vercel.app/",
   },
   {
     id: 3,
-    title: "Email Validator NPM Package",
+    title: "Rinterio - Design Home",
     description:
-      "An efficient and customizable NPM package for validating email addresses. Built using React.js and Node.js, it provides robust validation features to help developers ensure that email inputs meet required formats and standards.",
-    image: npmLogo,
-    tags: ["React JS", "Node.js", "NPM", "Validation"],
+      "I developed Rinterio - a home design website using DaisyUI and Tailwind CSS. The website features interactive product listings, responsive layouts and user-friendly navigation. I implemented dynamic content integration, enhanced UX with smooth animations and managed the project using Git/GitHub.",
+    image: house,
+    tags: ["HTML", "TailwindCSS", "daisyUI"],
     github: "https://github.com/codingmastr/cmtk-email-validator",
     webapp: "https://www.npmjs.com/package/cmtk-email-validator",
-  },
-  {
-    id: 4,
-    title: "Task Reminder Chrome Extension Tool",
-    description:
-      "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
-    image: taskremLogo,
-    tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
-    github: "https://github.com/codingmastr/Task-Reminder-Tool",
-    webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
-  },
-  {
-    id: 5,
-    title: "Webverse Digital",
-    description:
-      "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
-    image: webverLogo,
-    tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
-    github: "https://github.com/codingmastr/Webverse-Digital",
-    webapp: "https://webversedigital.com/",
-  },
-  {
-    id: 6,
-    title: "Coding Master",
-    description:
-      "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
-    image: cmLogo,
-    tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
-    github: "https://codingmasterweb.in/",
-    webapp: "https://codingmasterweb.in/",
-  },
-  {
-    id: 7,
-    title: "Image Search App",
-    description:
-      "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
-    image: imagesearchLogo,
-    tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
-    github: "https://github.com/codingmastr/Image-Search-App",
-    webapp: "https://imagsearch.netlify.app/",
-  },
-  {
-    id: 8,
-    title: "Image Background Remover",
-    description:
-      "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
-    image: removebgLogo,
-    tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
-    github: "https://github.com/codingmastr/Image-Background-Remover",
-    webapp: "https://removeyourbg.netlify.app/",
   },
 ];
